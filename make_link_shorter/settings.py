e9d31e1a10a293759ee4b3eb55329e2f32bd8fbf
Teiko <no.name.tyan.d@gmail.com>
2022-01-05 13:03:54 +0300

    changed secret key

--- make_link_shorter/settings.py
+++ make_link_shorter/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(4i8&o)zzip6=g_ywzbbsri$nh&5c#l84)vykgz0s&^qvk$c!)'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-(4i8&o)zzip6=g_ywzbbsri$nh&5c#l84)vykgz0s&^qvk$c!)')
 
 DEBUG = False
 
